e16554c3ee75a1f5e1cc1f77fe71b0c7b541d33c
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2019-10-15 16:48:35 -0400

    papa lui game is implemented but image casting is slow
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- project/firmware/main.c
+++ project/firmware/main.c
@@ -17,20 +17,23 @@ Graphics_Context g_sContext;
 /* ADC results buffer */
 static uint16_t resultsBuffer_ACC[3];
 static uint16_t resultsBuffer_JOY[2];
+
 uint8_t image[16384];
 uint32_t pallet[256];
+
 char codes[] = "START";
 int codeindex=0;
 int dummyflag = 0;
-char dataReceived[1000];
+
 int index = 0;
 int palletindex = 0;
 int imageindex = 0;
 int imageOrpallet = -1;//0 means image 1 means pallet
+//int updateLCD = 0;
 uint32_t palletData = 0;
 Graphics_Image bitmap;
-int delay = 0;
-int buttonstate = 0;
+
+
 const eUSCI_UART_Config uartConfig =
 {
     EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
@@ -44,7 +47,8 @@ const eUSCI_UART_Config uartConfig =
     EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
 };
 
-void num2str(uint16_t x, char* str);
+
+void processReceivedData(uint8_t data);
 
 enum AR_States { AR_SMStart, AR_IDEAL, AR_PRESS_LR, AR_PRESS_UD} ar_state;
 enum AR_States TickFct_Arrow(enum AR_States state);
@@ -52,6 +56,8 @@ enum AR_States TickFct_Arrow(enum AR_States state);
 enum BTN_States { BTN_SMStart, BTN_IDEAL, BTN_PRESS_SPACE, BTN_PRESS_X, BTN_PRESS_Z} btn_state;
 enum BTN_States TickFct_Button(enum BTN_States state);
 
+//enum IMG_States { IMG_SMStart, IMG_IDEAL, IMG_UPDATE} img_state;
+//enum IMG_States TickFct_Image(enum IMG_States state);
 
 int done = 0;
 int main(void)
@@ -69,6 +75,7 @@ int main(void)
 
     ar_state = AR_SMStart;
     btn_state = BTN_SMStart;
+//    img_state = IMG_SMStart;
 
     /* Set the core voltage level to VCORE1 */
     MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
@@ -95,7 +102,7 @@ int main(void)
       /* Setting DCO to 12MHz */
       CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
 
-      //![Simple UART Example]
+
       /* Configuring UART Module */
       MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
@@ -107,7 +114,7 @@ int main(void)
       MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
       MAP_Interrupt_enableSleepOnIsrExit();
       MAP_Interrupt_enableMaster();
-      //![Simple UART Example]
+
 
     /* Initializes display */
     Crystalfontz128x128_Init();
@@ -184,79 +191,18 @@ int main(void)
 
 
 
-/* EUSCI A0 UART ISR - Echoes data back to PC host */
+/* EUSCI A0 UART ISR */
 void EUSCIA0_IRQHandler(void)
 {
     uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
-
     MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
-
     uint8_t data;
     if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
     {
         data = MAP_UART_receiveData(EUSCI_A0_BASE);
-
-        if(codeindex==5)
-        {
-            codeindex=0;
-            if((char)data == 'I')
-            {
-                imageindex = 0;
-                imageOrpallet = 0;
-                dummyflag=1;
-            }
-            if((char)data == 'P')
-            {
-                palletindex = 0;
-                imageOrpallet = 1;
-                index = 0;
-                dummyflag=1;
-            }
-        }
-
-        if(codes[codeindex] == (char)data)
-        {
-            codeindex++;
-        }
-        else
-        {
-            codeindex = 0;
-        }
-
-
-        if(imageOrpallet==0 && dummyflag==0)
-        {
-            image[imageindex++] = data;
-            if(imageindex == 16384)
-            {
-                imageindex = 0;
-                imageOrpallet = -1;
-            }
-        }
-        else if(imageOrpallet==1 && dummyflag==0)
-        {
-            if (index == 0) {palletData |= ((uint32_t) data) << 16; index++;}
-            else if (index == 1) {palletData |= ((uint32_t) data) << 8; index++;}
-            else if (index == 2) {palletData |= ((uint32_t) data); index=0; pallet[palletindex]=palletData; palletData=0; palletindex++;}
-            if(palletindex==256)
-            {
-                palletindex = 0;
-                if(imageindex==0 && palletindex==0)
-                {
-                    Graphics_drawImage(&g_sContext, &bitmap, 0, 0);
-                }
-            }
-        }
-        dummyflag=0;
+        processReceivedData(data);
     }
-
 }
-
-
-
-
-
-
 
 /* This interrupt is fired whenever a conversion is completed and placed in
  * ADC_MEM2. This signals the end of conversion and the results array is
@@ -264,7 +210,6 @@ void EUSCIA0_IRQHandler(void)
 void ADC14_IRQHandler(void)
 {
     uint64_t status;
-
     status = MAP_ADC14_getEnabledInterruptStatus();
     MAP_ADC14_clearInterruptFlag(status);
 
@@ -285,6 +230,87 @@ void ADC14_IRQHandler(void)
         btn_state = TickFct_Button(btn_state);
     }
 }
+
+
+void processReceivedData(uint8_t data)
+{
+    if(codeindex==5)
+    {
+        codeindex=0;
+        if((char)data == 'I')
+        {
+            imageindex = 0;
+            imageOrpallet = 0;
+            dummyflag=1;
+        }
+        if((char)data == 'P')
+        {
+            palletindex = 0;
+            imageOrpallet = 1;
+            index = 0;
+            dummyflag=1;
+        }
+    }
+
+    if(codes[codeindex] == (char)data)
+    {
+        codeindex++;
+    }
+    else
+    {
+        codeindex = 0;
+    }
+
+    if(imageOrpallet==0 && dummyflag==0)
+    {
+        image[imageindex++] = data;
+        if(imageindex == 16384)
+        {
+            imageindex = 0;
+            imageOrpallet = -1;
+        }
+    }
+    else if(imageOrpallet==1 && dummyflag==0)
+    {
+        if (index == 0) {palletData |= ((uint32_t) data) << 16; index++;}
+        else if (index == 1) {palletData |= ((uint32_t) data) << 8; index++;}
+        else if (index == 2) {palletData |= ((uint32_t) data); index=0; pallet[palletindex]=palletData; palletData=0; palletindex++;}
+        if(palletindex==256)
+        {
+            palletindex = 0;
+            if(imageindex==0 && palletindex==0)
+            {
+                Graphics_drawImage(&g_sContext, &bitmap, 0, 0);
+            }
+        }
+    }
+    dummyflag=0;
+}
+
+//enum IMG_States TickFct_Image(enum IMG_States state)
+//{
+//    switch(state)
+//    {
+//        case IMG_SMStart:
+//            state = IMG_IDEAL;
+//            break;
+//        case IMG_IDEAL:
+//            if(updateLCD==1)
+//            {
+//                state = IMG_UPDATE;
+//            }
+//            break;
+//        case IMG_UPDATE:
+//            Graphics_drawImage(&g_sContext, &bitmap, 0, 0);
+//            state = IMG_IDEAL;
+//            updateLCD = 0;
+//            break;
+//        default:
+//            state = IMG_IDEAL;
+//            break;
+//    }
+//    return state;
+//}
 
 
 enum BTN_States TickFct_Button(enum BTN_States state)
@@ -434,16 +460,4 @@ enum AR_States TickFct_Arrow(enum AR_States state) {
         break;
    }
   return state;
-}
-
-
-
-void num2str(uint16_t x, char *str)
-{
-    int i;
-    for(i=15;(x != 0 && i >=0);i--)
-    {
-        str[i] = (char)('0'+ x%10);
-        x=x/10;
-    }
 }
--- project/src/parseData.py
+++ project/src/parseData.py
@@ -27,15 +27,20 @@ cursorImage = cursorImage.resize((20, 20), Image.ANTIALIAS)
 
 def serialSendThread():
     global ser, breakSerial, cursorImage
+    x0 = 450
+    y0 = 690
+    w = 530
+    h = 390
+
     if ser.is_open:
         with mss() as sct:
             monitor_number = 0
             mon = sct.monitors[monitor_number]
             monitor = {
-                "top": mon["top"] + 410,  # 100px from the top
-                "left": mon["left"] + 460,  # 100px from the left
-                "width": 640,
-                "height": 380,
+                "top": mon["top"] + x0,
+                "left": mon["left"] + y0,
+                "width": w,
+                "height": h,
                 "mon": monitor_number,
             }
             while not breakSerial:
@@ -45,8 +50,8 @@ def serialSendThread():
                 # print(mousex,mousey)
                 mousex, mousey = mousex-460, mousey-410
                 imgpill = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-                if ((mousex+20) <= 640) and ((mousey+20) <= 380 and mousex >=0 and mousey >= 0):
-                    imgpill.paste(cursorImage, box=(mousex,mousey,mousex+20,mousey+20))
+                # if ((mousex+20) <= 640) and ((mousey+20) <= 380 and mousex >=0 and mousey >= 0):
+                #     imgpill.paste(cursorImage, box=(mousex,mousey,mousex+20,mousey+20))
                 # imgpill.show()
                 imgpill = imgpill.resize((128, 128), Image.ANTIALIAS)
                 imgcvt = imgpill.convert('P')
@@ -54,12 +59,16 @@ def serialSendThread():
                 image = imgcvt.tobytes()
                 pallet = bytearray(pallet)
                 ser.write(b'STARTI')
-                time.sleep(0.2)
+                # time.sleep(0.2)
                 ser.write(image)
-                time.sleep(0.5)
+                # time.sleep(0.5)
                 ser.write(b'STARTP')
-                time.sleep(0.2)
+                # time.sleep(0.2)
                 ser.write(pallet)
+
+                # datareceive = ser.read_until()
+                # if datareceive:
+                #     decodeCode(datareceive)
                 time.sleep(0.5)
     print("finish send thread")
 
@@ -70,41 +79,44 @@ def serialReceiveThread():
         while not breakSerial:
             datareceive = ser.read_until()
             if datareceive:
-                try:
-                    code = datareceive.decode("utf-8")
-                    if code == "RIGHT_ARROW\n":
-                        pyautogui.keyDown('right')
-                    elif code == "LEFT_ARROW\n":
-                        pyautogui.keyDown('left')
-                    elif code == "LR_ARROW_UP\n":
-                        pyautogui.keyUp('left')
-                        pyautogui.keyUp('right')
-                    elif code == "UP_ARROW\n":
-                        pyautogui.keyDown('up')
-                    elif code == "DOWN_ARROW\n":
-                        pyautogui.keyDown('down')
-                    elif code == "UD_ARROW_UP\n":
-                        pyautogui.keyUp('up')
-                        pyautogui.keyUp('down')
-                    elif code == "SPACE_BAR\n":
-                        pyautogui.keyDown('space')
-                    elif code == "SPACE_BAR_UP\n":
-                        pyautogui.keyUp('space')
-                    elif code == "Z\n":
-                        pyautogui.keyDown('z')
-                    elif code == "Z_UP\n":
-                        pyautogui.keyUp('z')
-                    elif code == "X\n":
-                        pyautogui.keyDown('x')
-                    elif code == "X_UP\n":
-                        pyautogui.keyUp('x')
-                except Exception as e:
-                    print("error: "+str(e))
+                decodeCode(datareceive)
     print("finish revceive thread")
+
+def decodeCode(code):
+    try:
+        code = code.decode("utf-8")
+        if code == "RIGHT_ARROW\n":
+            pyautogui.keyDown('right')
+        elif code == "LEFT_ARROW\n":
+            pyautogui.keyDown('left')
+        elif code == "LR_ARROW_UP\n":
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('right')
+        elif code == "UP_ARROW\n":
+            pyautogui.keyDown('up')
+        elif code == "DOWN_ARROW\n":
+            pyautogui.keyDown('down')
+        elif code == "UD_ARROW_UP\n":
+            pyautogui.keyUp('up')
+            pyautogui.keyUp('down')
+        elif code == "SPACE_BAR\n":
+            pyautogui.keyDown('space')
+        elif code == "SPACE_BAR_UP\n":
+            pyautogui.keyUp('space')
+        elif code == "Z\n":
+            pyautogui.keyDown('z')
+        elif code == "Z_UP\n":
+            pyautogui.keyUp('z')
+        elif code == "X\n":
+            pyautogui.keyDown('x')
+        elif code == "X_UP\n":
+            pyautogui.keyUp('x')
+    except Exception as e:
+        print("error: " + str(e))
 
 
 if __name__ == "__main__":
-    # threading.Thread(target=serialSendThread).start()
+    threading.Thread(target=serialSendThread).start()
     threading.Thread(target=serialReceiveThread).start()
 
     while True:
