98c4131acc949be17d6319d3b3981b2c3014beaa
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2019-11-30 18:14:58 -0500

    comment is added to firmware and driver code
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- project/firmware/main.c
+++ project/firmware/main.c
@@ -11,14 +11,16 @@
 #include "LcdDriver/Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
 
-#define TOTAL_LCD_BYTES 1024
-#define PALLET_CHUNK    7
 
-#pragma DATA_ALIGN(controlTable, TOTAL_LCD_BYTES*2)
 
-uint8_t controlTable[TOTAL_LCD_BYTES];
-char RXData_ping[TOTAL_LCD_BYTES];
-char RXData_pong[TOTAL_LCD_BYTES];
+#define TOTAL_LCD_BYTES 1024 // total data received at a time from the serial port using DMA
+#define PALLET_CHUNK    7    // total chunk of data to get the whole image frame
+
+#pragma DATA_ALIGN(controlTable, TOTAL_LCD_BYTES*2) // align memory to copy the data from serial port to memory
+
+uint8_t controlTable[TOTAL_LCD_BYTES]; // to hold the received data
+char RXData_ping[TOTAL_LCD_BYTES]; // to hold received data buffer 1
+char RXData_pong[TOTAL_LCD_BYTES]; // to hold received data buffer 2
 
 
 
@@ -27,17 +29,19 @@ Graphics_Context g_sContext;
 
 /* ADC results buffer */
 
-static uint16_t resultsBuffer_JOY[2];
+static uint16_t resultsBuffer_JOY[2]; // to hold joystick data
 
-uint8_t image[16384];
-uint32_t pallet[256];
+uint8_t image[16384]; // to hold image data
+uint32_t pallet[256]; // to hold pallet data
 
+
+// some variable to manage the received data correctly
 int count = 0;
 int palletindex = 0;
 int imageindex = 0;
 bool sendsync = false;
 
-Graphics_Image bitmap;
+Graphics_Image bitmap; //bitmap image variable used by the graphics library
 
 
 // baudrate 460800 bps
@@ -57,25 +61,25 @@ const eUSCI_UART_ConfigV1 uartConfig =
 
 
 
-void init_CLOCK(void);
-void init_UART(void);
-void init_ADC(void);
-void init_GPIO(void);
-void init_DMA(void);
-void init_LCD(void);
-void init_SM(void);
+void init_CLOCK(void); // declaration of function to initialize clock system
+void init_UART(void);  // declaration of function to initialize UART system
+void init_ADC(void);   // declaration of function to initialize ADC system
+void init_GPIO(void);  // declaration of function to initialize GPIO
+void init_DMA(void);   // declaration of function to initialize DMA system
+void init_LCD(void);   // declaration of function to initialize LCD
+void init_SM(void);    // declaration of function to initialize state machine
 
-void process_ReceivedData(bool ping_or_pong);
-void send_Command(char *cmd);
+void process_ReceivedData(bool ping_or_pong); // declaration of function to process the data received using the DMA from UART module
+void send_Command(char *cmd); // declaration of function to send command to the host PC
 
-enum AR_States { AR_SMStart, AR_IDEAL, AR_PRESS_LR, AR_PRESS_UD} ar_state;
-enum AR_States TickFct_Arrow(enum AR_States state);
+enum AR_States { AR_SMStart, AR_IDEAL, AR_PRESS_LR, AR_PRESS_UD} ar_state; // states of the JoyStick input state machine
+enum AR_States TickFct_Arrow(enum AR_States state); // transition function of the JoyStick input state machine
 
-enum BTN_States { BTN_SMStart, BTN_IDEAL, BTN_PRESS_SPACE, BTN_PRESS_X, BTN_PRESS_Z} btn_state;
-enum BTN_States TickFct_Button(enum BTN_States state);
+enum BTN_States { BTN_SMStart, BTN_IDEAL, BTN_PRESS_SPACE, BTN_PRESS_X, BTN_PRESS_Z} btn_state; // states of the Button input state machine
+enum BTN_States TickFct_Button(enum BTN_States state); // transition function of the Button input state machine
 
 
-int done = 0;
+// main function of the firmware
 int main(void)
 {
     /* Halting WDT and disabling master interrupts */
@@ -92,12 +96,12 @@ int main(void)
 
     while(1)
     {
-        MAP_PCM_gotoLPM0();
+        MAP_PCM_gotoLPM0(); // enter into low power mode
     }
 }
 
 
-
+// initialize the clock system of the launchpad
 void init_CLOCK(void)
 {
     MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
@@ -112,6 +116,7 @@ void init_CLOCK(void)
     MAP_CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 }
 
+// initialize the three button used by the game controller
 void init_GPIO(void)
 {
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN5);
@@ -119,7 +124,7 @@ void init_GPIO(void)
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P4, GPIO_PIN1);
 }
 
-
+// initialize the BoosterPack's LCD to be update with the Current score of the game
 void init_LCD(void)
 {
     bitmap.bPP = 8;
@@ -138,13 +143,14 @@ void init_LCD(void)
     GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
 }
 
+// initialize the state machines initial state
 void init_SM(void)
 {
     ar_state = AR_SMStart;
     btn_state = BTN_SMStart;
 }
 
-
+// initialize serial port of the launchpad
 void init_UART(void)
 {
     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
@@ -152,7 +158,7 @@ void init_UART(void)
     MAP_UART_enableModule(EUSCI_A0_BASE);
 }
 
-
+// initialize the analog to digital communication to get the JoyStick movement
 void init_ADC(void)
 {
 
@@ -178,9 +184,7 @@ void init_ADC(void)
     MAP_ADC14_toggleConversionTrigger();
 }
 
-
-
-
+// initialize the DMA for ping-pong mode
 void init_DMA(void)
 {
     /* Configuring DMA module */
@@ -204,8 +208,7 @@ void init_DMA(void)
     MAP_Interrupt_enableSleepOnIsrExit();
 }
 
-
-
+// process the data received from serial port using DMA in DMA interrupt sub-routine
 void DMA_INT1_IRQHandler(void)
 {
     sendsync = true;
@@ -225,10 +228,7 @@ void DMA_INT1_IRQHandler(void)
     printf("data comming  %d\n", count);
  }
 
-
-
-
-
+// Process the ADC data and execute the state machines' tick function
 void ADC14_IRQHandler(void)
 {
     uint64_t status;
@@ -247,7 +247,7 @@ void ADC14_IRQHandler(void)
     }
 }
 
-
+// copy the image data to a global variable and update the LCD
 void process_ReceivedData(bool ping_or_pong)
 {
     int i;
@@ -287,6 +287,7 @@ void process_ReceivedData(bool ping_or_pong)
     }
 }
 
+// send a command string to the host PC using serial port
 void send_Command(char *cmd)
 {
     while(*cmd != 0)
@@ -295,6 +296,7 @@ void send_Command(char *cmd)
     }
 }
 
+// button state tick function
 enum BTN_States TickFct_Button(enum BTN_States state)
 {
     switch(state)
@@ -347,6 +349,7 @@ enum BTN_States TickFct_Button(enum BTN_States state)
     return state;
 }
 
+// Joystick state tick function
 enum AR_States TickFct_Arrow(enum AR_States state) {
   switch(state)
   {
@@ -376,14 +379,14 @@ enum AR_States TickFct_Arrow(enum AR_States state) {
          }
         break;
      case AR_PRESS_LR:
-         if(resultsBuffer_JOY[0]>6500 && resultsBuffer_JOY[0] <8000)
+         if(resultsBuffer_JOY[0]>6500 && resultsBuffer_JOY[0]<8000)
          {
              send_Command("LR_ARROW_UP\n");
              state = AR_IDEAL;
          }
          break;
      case AR_PRESS_UD:
-         if(resultsBuffer_JOY[1]>7500 && resultsBuffer_JOY[1] <9000)
+         if(resultsBuffer_JOY[1]>7500 && resultsBuffer_JOY[1]<9000)
          {
              send_Command("UD_ARROW_UP\n");
              state = AR_IDEAL;
@@ -395,3 +398,5 @@ enum AR_States TickFct_Arrow(enum AR_States state) {
    }
   return state;
 }
+
+/**************************** END Firmware Code ****************************/
--- project/src/parseData.py
+++ project/src/parseData.py
@@ -14,23 +14,22 @@ from mss.darwin import MSS as mss
 
 # GNU/Linux
 # from mss.linux import MSS as mss
-from math import atan2, cos, sin
 
 # Microsoft Windows
 # from mss.windows import MSS as mss
 
-
+# send screenshot image to launchpad
 def serialSendThread():
     global ser, breakSerial, cursorImage, sendimageflag
-    x0 = 645 #300
-    y0 = 450
-    w = 530
-    h = 50 #390
-    datachunksize = 1024
+    x0 = 645  # top left x co-ordinate of the screen to be captured
+    y0 = 450  # top left y co-ordinate of the screen to be captured
+    w = 530   # width of the captured screen
+    h = 50    # height of the captured screen
+    datachunksize = 1024  # number of bytes sent to the launchpad at a time
     newPallet = [0]*datachunksize
-    if ser.is_open:
+    if ser.is_open: # check if serial port is open or not
         with mss() as sct:
-            monitor_number = 0
+            monitor_number = 0 # number of the monitor for which the screenshot will be captured
             mon = sct.monitors[monitor_number]
             monitor = {
                 "top": mon["top"] + x0,
@@ -39,35 +38,36 @@ def serialSendThread():
                 "height": h,
                 "mon": monitor_number,
             }
-            templateImage = Image.new("RGB",(265,100))
+            templateImage = Image.new("RGB",(265,100)) # a template image on top of it the processed image will be pasted
             while not breakSerial:
                 if sendimageflag:
-                    sct_img = sct.grab(monitor)
-                    imgpill = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-                    leftHalfimg = imgpill.crop((0,0,265,50))
-                    rightHalfimg = imgpill.crop((265, 0, 530, 50))
-                    templateImage.paste(leftHalfimg,(0,0))
-                    templateImage.paste(rightHalfimg, (0, 50))
-                    resizedimg = templateImage.resize((128, 48), Image.ANTIALIAS)
-                    imgcvt = resizedimg.convert('P')
-                    pallet = imgcvt.getpalette()
+                    sct_img = sct.grab(monitor) # capture a screenshot
+                    imgpill = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX") # convert the captured image into PIL format
+                    leftHalfimg = imgpill.crop((0,0,265,50)) # take the left half of the image
+                    rightHalfimg = imgpill.crop((265, 0, 530, 50)) # take the right half of the image
+                    templateImage.paste(leftHalfimg,(0,0)) # paste the left half to the top of the template image
+                    templateImage.paste(rightHalfimg, (0, 50)) # paste the right half to the bottom of the template image
+                    resizedimg = templateImage.resize((128, 48), Image.ANTIALIAS) # resize the template image to lower resolution
+                    imgcvt = resizedimg.convert('P') # convert the image to pallet-indexed image
+                    pallet = imgcvt.getpalette() # get the pallet of the converted image
                     pindx = 0
-                    for ii in range(len(pallet)):
+                    for ii in range(len(pallet)): # format the pallet to consider the alpha channel as zero
                         if ii%3==0:
                             pindx += 1
                         newPallet[pindx] = pallet[ii]
                         pindx += 1
-                    image = imgcvt.tobytes()
-                    pallet = bytearray(newPallet)
-                    for indx in range(0, len(image), datachunksize):
+                    image = imgcvt.tobytes() # convert the image to byte array
+                    pallet = bytearray(newPallet) # convert the pallet to byte array
+                    for indx in range(0, len(image), datachunksize): # sent 1024 bytes of the image at a time
                         sendBuffer = image[indx:indx+datachunksize]
-                        ser.write(sendBuffer)
-                        time.sleep(0.2) #0.25 was best
-                    ser.write(pallet)
-                    time.sleep(0.2) #0.25 was best
+                        ser.write(sendBuffer) # sent data to the launchpad through serial port
+                        time.sleep(0.2) # a small delay
+                    ser.write(pallet) # send the pallet of the image
+                    time.sleep(0.2) # a small delay
 
     print("finish send thread")
 
+# receive command code from launchpad to be decoded and executed
 def serialReceiveThread():
     global ser, breakSerial
     if ser.is_open:
@@ -77,6 +77,7 @@ def serialReceiveThread():
                 decodeCode(datareceive)
     print("finish revceive thread")
 
+# decode the received unique code and send key stroke to the operating system
 def decodeCode(code):
     global sendimageflag
     try:
@@ -112,19 +113,21 @@ def decodeCode(code):
     except Exception as e:
         print("error: " + str(e))
 
+# main thread
 if __name__ == "__main__":
+    # create a serial object
     # ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) #for linux
     ser = serial.Serial('/dev/tty.usbmodemM43210051', 460800, timeout=1) #for mac
     breakSerial = False
     sendimageflag = False
-    threading.Thread(target=serialSendThread).start()
-    threading.Thread(target=serialReceiveThread).start()
+    threading.Thread(target=serialSendThread).start()       # start serial transmit thread
+    threading.Thread(target=serialReceiveThread).start()    # start serial receive thread
     while True:
-        option = input("input \"q\" for quite:\n")
+        option = input("input \"q\" for quite:\n")  # wait for user input
         if option == 'q':
             breakSerial = True
             time.sleep(1)
             break
-    ser.close()
+    ser.close()   # close serial communication
 
 
